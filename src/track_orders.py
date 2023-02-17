3850984c3dfcd0c97ce8bff4dc654806de2adab0
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-02-17 08:05:40 -0300

    req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,82 @@
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = list()
+        self._custormesr = set()
+        self._dishes = set()
+        self._weekdays = set()
+
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def every_weekday_worked(self):
+        return [
+            order['weekday']
+            for order in self.orders
+        ]
+
+    def most_frequently(self, list: list):
+        most_common_item = list[0]
+        frequency = dict()
+        for item in list:
+            if item not in frequency:
+                frequency[item] = 1
+            else:
+                frequency[item] += 1
+            if frequency[item] > frequency[most_common_item]:
+                most_common_item = item
+
+        return most_common_item
+
+    def less_frequent(self, list: list):
+        most_common_item = list[0]
+        frequency = dict()
+        for item in list:
+            if item not in frequency:
+                frequency[item] = 1
+            else:
+                frequency[item] += 1
+            if frequency[item] < frequency[most_common_item]:
+                most_common_item = item
+
+        return most_common_item
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append({
+            "customer": customer,
+            "dish": order,
+            "weekday": day
+            })
+        self._custormesr.add(customer)
+        self._dishes.add(order)
+        self._weekdays.add(day)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        dishes = [
+            order['dish']
+            for order in self.orders
+            if order['customer'] == customer
+        ]
+
+        return self.most_frequently(dishes)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        dishes = set(
+            order['dish']
+            for order in self.orders
+            if order['customer'] == customer
+        )
+
+        return self._dishes - dishes
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        weekdays = set(
+            order['weekday']
+            for order in self.orders
+            if order['customer'] == customer
+        )
+
+        return self._weekdays - weekdays
 
     def get_busiest_day(self):
         pass
